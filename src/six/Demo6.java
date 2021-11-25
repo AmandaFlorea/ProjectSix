a612f42c510bda5191d949fb0c714df395532231
AmandaFlorea <94856319+AmandaFlorea@users.noreply.github.com>
2021-11-24 20:46:14 -0500

    Update Demo6.java
    changes made in github

--- src/six/Demo6.java
+++ src/six/Demo6.java
@@ -3,6 +3,8 @@ package six;
 public class Demo6 {
 	public static void main(String[] args) {
 		System.out.println("DEMO 6 push to repository");
+		
+		System.out.println("change made from github");
 	}
 
 }
